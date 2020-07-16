1435caf37a0c142cd865b1c118fcf961568f4429
drash7 <43657207+drash7@users.noreply.github.com>
2020-07-17 02:18:32 +0530

    test commit

---
+++ Assets/_Project/Scripts/FinalTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinalTrigger : MonoBehaviour
+{
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag.Equals("Load"))
+        {
+            ScoresController.Instance.addScore();
+            ScoresController.Instance.respawnLoad();
+        }
+    }
+}
---
+++ Assets/_Project/Scripts/ScoresController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoresController : Singleton<ScoresController>
+{
+
+    public Text scoreText;
+    private int currentScore;
+    public GameObject loadPrefab;
+    public GameObject load;
+    private Transform initLoadPos;
+
+    void Start()
+    {
+        currentScore = 0;
+        initLoadPos = load.transform;
+        load = GameObject.FindWithTag("Load");
+    }
+
+    public void addScore()
+    {
+        currentScore += 1;
+        scoreText.text = "Score: " + currentScore.ToString();
+        // Destroy(load);
+    }
+
+    public void respawnLoad()
+    {
+        load.transform.position = initLoadPos.position;
+    }
+}
